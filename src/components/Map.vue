82335691525decb1e3988898bf8a47d76799768a
Sahel Mohammad Iqbal <sahel13miqbal@gmail.com>
2020-10-20 12:11:09 +0530

    Gatsby project initialized

--- src/components/Map.vue
+++
@@ -1,8 +0,0 @@
-<template lang="pug">
-iframe(src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3745.110106531732!2d85.68116336491845!3d20.17114053646912!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a19aea7a433ea4b%3A0x455d13f7d6284a15!2sNational%20Institute%20of%20Science%20Education%20and%20Research!5e0!3m2!1sen!2sin!4v1602610187479!5m2!1sen!2sin' width='600' height='450' frameborder='0' style='border:0;' allowfullscreen='' aria-hidden='false' tabindex='0')
-</template>
-<script>
-export default {
-  name: "Map"
-}
-</script>
