894e1ae5149a02d1b89ca8c57cf1df71613f5d87
Sahel13 <sahel13miqbal@gmail.com>
2020-10-23 15:18:15 +0530

    About page created

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,9 +1,11 @@
 import React from "react"
+import { Link } from "gatsby"
 
 export default function About() {
   return (
     <div>
       <h1>This is the about page.</h1>
+      <p><Link to="/">Home</Link></p>
     </div>
   )
 }
