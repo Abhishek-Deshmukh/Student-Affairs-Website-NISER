181897b6ff0f4c00beb05bfba5565df4a0c3a8db
Sahel13 <sahel13miqbal@gmail.com>
2020-10-23 15:51:42 +0530

    The pages are now rendered inside the Layout component

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,13 @@
 import React from "react"
 import { Link } from "gatsby"
 
+import Layout from "../components/Layout"
+
 export default function About() {
   return (
-    <div>
+    <Layout>
       <h1>This is the about page.</h1>
       <p><Link to="/">Home</Link></p>
-    </div>
+    </Layout>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,14 +2,15 @@ import React from "react"
 import { Link } from "gatsby"
 
 import { useSiteMetadata } from "../hooks/use-site-metadata"
+import Layout from "../components/Layout"
 
 export default function Home() {
   const { title, description } = useSiteMetadata()
   return (
-    <React.Fragment>
+    <Layout>
       <h1>Welcome to {title}</h1>
       <p>{description}</p>
       <p> Link to the <Link to="/about/">about page</Link>.</p>
-    </React.Fragment>
+    </Layout>
   )
 }
