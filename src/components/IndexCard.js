7f319a0232783444aa6bd6cd0afc09a8ade09bee
Abhishek Anil Deshmukh <deshmukh.abhishek369@gmail.com>
2020-10-29 13:18:40 +0530

    fixed responsive 🕷s from home

--- src/components/IndexCard.js
+++ src/components/IndexCard.js
@@ -1,9 +1,12 @@
 import React from "react"
 import { Link } from "gatsby"
+import useWindowDimensions from "./useWindowDimensions";
 
 export default function IndexCard(props) {
-  var style_class = "index-card col-sm-6 col-md-4 col-lg-3"
-  if (props.index % 4 !== 3) {
+  var style_class = "index-card col-12 col-sm-12 col-md-4 col-lg-3"
+  const {height, width} = useWindowDimensions();
+  var num_cards_row = 1 + 2*(width >= 768) + (width >= 992);
+  if (props.index % num_cards_row !== (num_cards_row - 1)) {
     style_class += " border-right"
   }
   return (
