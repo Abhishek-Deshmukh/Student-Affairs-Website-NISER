7aba7702a9dad671b507f0497c0cd8e74d5dbdce
Sahel Mohammad Iqbal <sahel13miqbal@gmail.com>
2020-10-31 15:19:04 +0530

    filesource directory changed

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,7 +15,7 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        path: `${__dirname}/src/assets/`,
+        path: `${__dirname}/src`,
       },
     },
     `gatsby-transformer-remark`,
