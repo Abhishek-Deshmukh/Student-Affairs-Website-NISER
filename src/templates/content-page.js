3b603776d7d62f2aa75da97bcd1f14fda5aa9e00
Abhishek Anil Deshmukh <deshmukh.abhishek369@gmail.com>
2020-10-31 21:11:00 +0530

    basic styling in the template

--- src/templates/content-page.js
+++ src/templates/content-page.js
@@ -6,9 +6,14 @@ export default function ContentPage({ data }) {
   const post = data.markdownRemark
   return (
     <Layout>
-      <div>
-        <h1>{post.frontmatter.title}</h1>
-        <div dangerouslySetInnerHTML={{ __html: post.html }} />
+      <div className="article-container container-fluid row">
+        <aside className="col-2">
+          <input type="text" placeholder="Search"/>
+        </aside>
+        <div className="col-9">
+          <h1>{post.frontmatter.title}</h1>
+          <div dangerouslySetInnerHTML={{ __html: post.html }} />
+        </div>
       </div>
     </Layout>
   )
