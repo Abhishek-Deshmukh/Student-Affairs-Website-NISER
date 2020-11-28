1525afa50d47c2b811f550d444e1f07d9ba07d3a
Abhishek Anil Deshmukh <deshmukh.abhishek369@gmail.com>
2020-11-28 16:13:43 +0530

    add in the map

--- src/templates/content-page.js
+++ src/templates/content-page.js
@@ -4,7 +4,11 @@ import Layout from "../components/Layout"
 import order from "../contents/order.json"
 
 export default function ContentPage({ data }) {
-  const post = data.markdownRemark
+  var post = data.markdownRemark
+  // the map page
+  if (data.markdownRemark.frontmatter.title === "NISER Detailed Map") {
+    post.html = '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3745.1711091420616!2d85.68115061491841!3d20.168599886470606!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a19aea875ebf9fd%3A0x44948bdac82e20ba!2sNISER%20JATNI%20MAIN%20GATE!5e0!3m2!1sen!2sin!4v1606559679721!5m2!1sen!2sin" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>'
+  }
   const headings = order.map(x => {
     return {
       heading: x,
