4e9244e566d89e22ce1eebc2ad60d091740e4c97
Abhishek Anil Deshmukh <deshmukh.abhishek369@gmail.com>
2020-12-10 21:20:17 +0530

    fixed dividers on index page

--- src/components/IndexCard.js
+++ src/components/IndexCard.js
@@ -1,18 +1,10 @@
 import React from "react"
 import { Link } from "gatsby"
 import Img from "gatsby-image"
-import useWindowDimensions from "./useWindowDimensions"
 
 export default function IndexCard(props) {
-  var style_class = "index-card col-12 col-sm-12 col-md-4 col-lg-3"
-  const { height, width } = useWindowDimensions()
-  var num_cards_row = 1 + 2 * (width >= 768) + (width >= 992)
-  if (props.index % num_cards_row !== num_cards_row - 1) {
-    style_class += " border-right"
-  }
-
   return (
-    <div className={style_class}>
+    <div className="index-card col-12 col-sm-12 col-md-4 col-lg-3">
       <div className="center">
         {props.context.image ? (
           <Img fixed={props.context.image.childImageSharp.fixed} />
