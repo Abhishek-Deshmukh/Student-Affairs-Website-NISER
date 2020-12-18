395d5a69e84290d369c71aa76405d18aaad858cc
abhishek.deshmukh <deshmukh.abhishek369@gmail.com>
2020-12-18 10:23:06 +0530

    silented errors

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -22,44 +22,42 @@ const SEO = ({ title, description, home }) => {
     siteTitle,
     siteDescription,
     // siteUrl,
-    } = data.site.siteMetadata
+  } = data.site.siteMetadata
 
   // const siteLogo = data.file.publicURL
 
   const seo = {
-      title: title || siteTitle,
-      description: description || siteDescription,
-      // image: `${siteUrl}${image || siteLogo}`,
-      // url: `${siteUrl}${pathname}`,
+    title: title || siteTitle,
+    description: description || siteDescription,
+    // image: `${siteUrl}${image || siteLogo}`,
+    // url: `${siteUrl}${pathname}`,
   }
 
   let pageTitle
   if (title == null) {
     pageTitle = siteTitle
   } else {
-      pageTitle = `${seo.title} | ${siteTitle}`
+    pageTitle = `${seo.title} | ${siteTitle}`
   }
 
   return (
-      <Helmet title={pageTitle}>
-
-          // <link rel="canonical" href={seo.url} />
-          <meta name="description" content={seo.description} />
-          // <meta name="image" content={seo.image} />
-
-          <meta property="og:site_name" content={siteTitle} />
-          //<meta property="og:url" content={seo.url} />
-          <meta property="og:type" content={"website"} />
-          <meta property="og:title" content={seo.title} />
-          <meta property="og:description" content={seo.description} />
-          //<meta property="og:image" content={seo.image} />
-
-          //<meta name="twitter:card" content={home? "summary_large_image" : "summary"} />
-          <meta name="twitter:title" content={seo.title} />
-          <meta name="twitter:description" content={seo.description} />
-          //<meta name="twitter:image" content={seo.image} />
-
-      </Helmet>
+    <Helmet title={pageTitle}>
+      {
+        // <link rel="canonical" href={seo.url} />
+        // <meta name="image" content={seo.image} />
+        //<meta property="og:url" content={seo.url} />
+        //<meta property="og:image" content={seo.image} />
+        //<meta name="twitter:card" content={home? "summary_large_image" : "summary"} />
+        //<meta name="twitter:image" content={seo.image} />
+      }
+      <meta name="description" content={seo.description} />
+      <meta property="og:site_name" content={siteTitle} />
+      <meta property="og:type" content={"website"} />
+      <meta property="og:title" content={seo.title} />
+      <meta property="og:description" content={seo.description} />
+      <meta name="twitter:title" content={seo.title} />
+      <meta name="twitter:description" content={seo.description} />
+    </Helmet>
   )
 }
 
